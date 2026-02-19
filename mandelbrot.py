beedac32c986f49e2eb625a92a4fc412558f0e79
Søren Sørensen <h4.soren@gmail.com>
2026-02-19 12:12:31 +0100

    Uden numpy

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,7 +4,6 @@ Course: Numerical Scientific Computing 2026
 """
 import cmath
 import time 
-import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x): 
@@ -33,10 +32,14 @@ def mandelbrot_point(c, max_iter):
         z = z*z + c
     return max_iter
 
+def transpose(matrix):
+    """Transpose a 2D list."""
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
 def mandelbrot_set(xmin, xmax, ymin, ymax, width, height, max_iter):
-    r1 = np.linspace(xmin, xmax, width)
-    r2 = np.linspace(ymin, ymax, height)
-    return (r1, r2, np.array([[mandelbrot_point(complex(r, i), max_iter) for r in r1] for i in r2]))
+    r1 = [xmin + (xmax - xmin) * i / (width - 1) for i in range(width)]
+    r2 = [ymin + (ymax - ymin) * i / (height - 1) for i in range(height)]
+    return (r1, r2, [[mandelbrot_point(complex(r, i), max_iter) for r in r1] for i in r2])
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -44,5 +47,10 @@ if __name__ == "__main__":
     width, height = 1024, 1024
     max_iter = 100
     r1, r2, mandelbrot_image = mandelbrot_set(xmin, xmax, ymin, ymax, width, height, max_iter)
+    plt.imshow(transpose(mandelbrot_image), extent=(xmin, xmax, ymin, ymax))
+    plt.xlabel('Re')
+    plt.ylabel('Im')
+    plt.title('Mandelbrot Set')
+    plt.show()
     end_time = time.time()
     print(f"Tid for kode: {end_time - start_time} sekunder")
