e1d6fd39da6c2d2d03512165dde4cbbe66a36006
Søren Sørensen <h4.soren@gmail.com>
2026-02-19 12:35:19 +0100

    change of timing place

--- mandelbrot.py
+++ mandelbrot.py
@@ -44,10 +44,11 @@ if __name__ == "__main__":
     width, height = 1024, 1024
     max_iter = 100
     r1, r2, mandelbrot_image = mandelbrot_set(xmin, xmax, ymin, ymax, width, height, max_iter)
+    end_time = time.time()
+    print(f"Tid for kode: {end_time - start_time} sekunder")
     plt.imshow(mandelbrot_image, extent=(xmin, xmax, ymin, ymax))
     plt.xlabel('Re')
     plt.ylabel('Im')
     plt.title('Mandelbrot Set')
     plt.show()
-    end_time = time.time()
-    print(f"Tid for kode: {end_time - start_time} sekunder")
+    
