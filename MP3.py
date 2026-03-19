99b344ffdd9e3711af99b238365ca8c99b2861ef
Søren Sørensen <h4.soren@gmail.com>
2026-03-19 12:54:04 +0100

    Implementation of M2M1- parallelisering af koden

---
+++ MP3.py
@@ -0,0 +1,32 @@
+# mandelbrot_parallel.py (Tasks 1-3 are one continuous script)
+import numpy as np
+from numba import njit
+from multiprocessing import Pool
+import time, os, statistics, matplotlib.pyplot as plt
+from pathlib import Path
+
+@njit
+def mandelbrot_pixel(c_real, c_imag, max_iter):
+    z_real = z_imag = 0.0
+    for i in range(max_iter):
+        zr2 = z_real*z_real
+        zi2 = z_imag*z_imag
+        if zr2 + zi2 > 4.0: return i
+        z_imag = 2.0*z_real*z_imag + c_imag
+        z_real = zr2 - zi2 + c_real
+    return max_iter
+
+@njit
+def mandelbrot_chunk(row_start, row_end, N,
+                    x_min, x_max, y_min, y_max, max_iter):
+    out = np.empty((row_end - row_start, N), dtype=np.int32)
+    dx = (x_max - x_min) / N
+    dy = (y_max - y_min) / N
+    for r in range(row_end - row_start):
+        c_imag = y_min + (r + row_start) * dy
+        for col in range(N):
+            out[r, col] = mandelbrot_pixel(x_min + col*dx, c_imag, max_iter)
+    return out
+
+def mandelbrot_serial(N, x_min, x_max, y_min, y_max, max_iter=100):
+    return mandelbrot_chunk(0, N, N, x_min, x_max, y_min, y_max, max_iter)
